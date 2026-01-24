65ff61d34d48c3be0db303ba1a2202d3e9ad1621
Kevin Rohan Vaz <excalibur.krv@gmail.com>
2026-01-25 01:18:01 +0530

    chore: add github pages scripts

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,25 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-  preprocess: vitePreprocess()
-}
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/kevinrvaz.engineer' : '',
+        },
+    }
+};
+
+export default config;
